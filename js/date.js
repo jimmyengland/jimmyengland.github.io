ead8f331cfa993d0c46ce125db302530eb277a56
Jimmy <eng15005@byui.edu>
2020-05-09 10:43:02 -0700

    W03 Project

--- js/date.js
+++ js/date.js
@@ -5,7 +5,12 @@ try {
         month: "long",
         year: "numeric"
     };
-    document.getElementById("lastUpdated").textContent = document.lastModified}
-    catch (e) {
-        alert("Error with code or your browser does not support Locale")
-    }
+    document.getElementById("lastUpdated").textContent = document.lastModified
+}
+catch (e) {
+    alert("Error with code or your browser does not support Locale")
+}
+
+var date = new Date();
+var year = date.getFullYear();
+document.getElementById("year").textContent = year
