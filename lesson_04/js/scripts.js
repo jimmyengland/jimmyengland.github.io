bad9cdc81cba352fa57af2c5c695cbbd03ef8654
Jimmy <eng15005@byui.edu>
2020-05-15 14:34:25 -0700

    Week4_03

--- lesson_04/js/scripts.js
+++ lesson_04/js/scripts.js
@@ -5,11 +5,12 @@ function toggleMenu() {
 var date = new Date();
 var year = date.getFullYear();
 var day = date.getDay();
+var dayNum = date.getDate();
 var month = date.getMonth();
 var monthNames = ["January","February","March","April","May","June","July","August","October","November","December"]
 var dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 
-var dateString = dayNames[day] + ", " + monthNames[month] + " " + day + ", " + year;
+var dateString = dayNames[day] + ", " + dayNum + " " + monthNames[month] + ", " + year;
 document.getElementById("date").innerHTML = dateString;
 
 
