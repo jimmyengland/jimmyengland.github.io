b734915e33cd90c2d6487eb8b485e3dee9bf958b
Jimmy <eng15005@byui.edu>
2020-06-02 17:28:30 -0700

    week07_01

---
+++ lesson_07/lazyload/lazyload.js
@@ -0,0 +1,45 @@
+const images = document.querySelectorAll('[data-src]');
+
+// const loadImages = (image) => {
+//     image.setAttribute('src', image.getAttribute('data-src'));
+//     image.onload = () => {
+//       image.removeAttribute('data-src');
+//     };
+//   };
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if (!src) {
+        return;
+    }
+
+    img.src = src;
+    img.removeAttribute("data-src")
+}
+
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 20px 0px"
+};
+
+if ('IntersectionObserver' in window) {
+    const imgObserver = new IntersectionObserver((items, imgObserver) => {
+        items.forEach((item) => {
+            if (!item.isIntersecting) {
+                return;
+            } else {
+                preloadImage(item.target);
+                imgObserver.unobserve(item.target);
+            }
+        });
+    }, imgOptions);
+
+    images.forEach((img) => {
+        imgObserver.observe(img);
+    });
+} else {
+    imagesToLoad.forEach((img) => {
+        loadImages(img);
+    });
+}
+
